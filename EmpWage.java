66acde88ed205b01eda1d9066ab39808cb81a7a6
kajal <tiwarikajal1306@gmail.com>
2020-05-11 22:59:57 +0530

    Welcome msg

---
+++ EmpWage.java
@@ -0,0 +1,11 @@
+package javaPrograms;
+
+public class EmpWage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Welcome to Employee Wage computation");
+	}
+
+}
