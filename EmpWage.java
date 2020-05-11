57b77e1e3891d34b1bf33b5e37532b440c3286e0
kajal <tiwarikajal1306@gmail.com>
2020-05-11 23:03:42 +0530

    Check employee present absent

--- EmpWage.java
+++ EmpWage.java
@@ -6,6 +6,13 @@ public class EmpWage {
 		// TODO Auto-generated method stub
 
 		System.out.println("Welcome to Employee Wage computation");
+		 int isfulltime=1;
+         double empcheck=Math.floor(Math.random() * 10 )%2;
+         if (empcheck==isfulltime)
+                 System.out.println("Employee is present");
+         else
+                 System.out.println("Employee is Absent");
+
 	}
 
 }
