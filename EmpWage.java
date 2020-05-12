b0ce821fe910de7d875c65151624eac96dbad0a6
kajal <tiwarikajal1306@gmail.com>
2020-05-12 09:58:48 +0530

    Solving using case statement

--- EmpWage.java
+++ EmpWage.java
@@ -1,23 +1,27 @@
 package javaPrograms;
 
 public class EmpWage {
+	public static final int IS_PART_TIME=1;
+	public static final	int IS_FULL_TIME=2;
+	public static final int  EMP_RATE_PER_HOUR=20;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		System.out.println("Welcome to Employee Wage computation");
-		int IS_PART_TIME=1;
-		int IS_FULL_TIME=2;
-        int  EMP_RATE_PER_HOUR=20;
         int empHrs=0;
         int empWage=0;
-        double empcheck=Math.floor(Math.random() * 10 )%3;
-        if (empcheck==IS_PART_TIME)
+        int empcheck=(int)Math.floor(Math.random() * 10 )%3;
+        switch (empcheck) {
+        case IS_PART_TIME:
         		empHrs=4;
-        else if (empcheck==IS_FULL_TIME)
+        		break;
+        case IS_FULL_TIME:
                 empHrs=8;
-        else
+                break;
+        default:
                 empHrs=0;
+        }
         empWage=empHrs * EMP_RATE_PER_HOUR;
         System.out.println("Emp Wage: " +empWage );
 	}
