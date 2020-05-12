7b94051b8678c94aa1d6274e243115f491337b93
kajal <tiwarikajal1306@gmail.com>
2020-05-12 09:27:29 +0530

    calculated part time and full time salary

--- EmpWage.java
+++ EmpWage.java
@@ -6,12 +6,15 @@ public class EmpWage {
 		// TODO Auto-generated method stub
 
 		System.out.println("Welcome to Employee Wage computation");
-		int IS_FULL_TIME=1;
+		int IS_PART_TIME=1;
+		int IS_FULL_TIME=2;
         int  EMP_RATE_PER_HOUR=20;
         int empHrs=0;
         int empWage=0;
-        double empcheck=Math.floor(Math.random() * 10 )%2;
-        if (empcheck==IS_FULL_TIME)
+        double empcheck=Math.floor(Math.random() * 10 )%3;
+        if (empcheck==IS_PART_TIME)
+        		empHrs=4;
+        else if (empcheck==IS_FULL_TIME)
                 empHrs=8;
         else
                 empHrs=0;
