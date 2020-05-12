bff5158c354b1335b8216bd6d5e436e4ff2428f5
kajal <tiwarikajal1306@gmail.com>
2020-05-12 10:57:45 +0530

    Caluted wages till condition satisfied

--- EmpWage.java
+++ EmpWage.java
@@ -5,13 +5,15 @@ public class EmpWage {
 	public static final	int IS_FULL_TIME=2;
 	public static final int  EMP_RATE_PER_HOUR=20;
 	public static final int NUM_OF_WORKING_DAYS=20;
+	public static final int MAX_HRS_IN_MONTH=100;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int empHrs=0;
-        int empWage=0;
-        int totalEmpWage=0;
+        int totalWorkingDays=0;
+        int totalEmpHrs=0;
 		System.out.println("Welcome to Employee Wage computation");
-		for (int day=0; day < NUM_OF_WORKING_DAYS; day++) {
+		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUM_OF_WORKING_DAYS) {
+			totalWorkingDays++;
         int empcheck=(int)Math.floor(Math.random() * 10 )%3;
         switch (empcheck) {
         case IS_PART_TIME:
@@ -23,10 +25,10 @@ public class EmpWage {
         default:
                 empHrs=0;
         }
-        empWage=empHrs * EMP_RATE_PER_HOUR;
-        totalEmpWage=totalEmpWage+empWage;
-        System.out.println("Emp Wage: " +empWage );
+        totalEmpHrs +=empHrs;
+        System.out.println("Day: " +totalWorkingDays + "Emp Hr:" +empHrs);
 	}
+		int totalEmpWage=totalEmpHrs * EMP_RATE_PER_HOUR;
         System.out.println("Total Emp Wage:" +totalEmpWage);
 }
 }
