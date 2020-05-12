7a552514ae1356a169ae2d5e4b049dc20b10053a
kajal <tiwarikajal1306@gmail.com>
2020-05-12 12:10:34 +0530

    refractor code to write a class method to compute a wage

--- EmpWage.java
+++ EmpWage.java
@@ -1,18 +1,16 @@
 package javaPrograms;
-
-public class EmpWage {
+class EmpWageCalculate{
 	public static final int IS_PART_TIME=1;
 	public static final	int IS_FULL_TIME=2;
 	public static final int  EMP_RATE_PER_HOUR=20;
 	public static final int NUM_OF_WORKING_DAYS=20;
 	public static final int MAX_HRS_IN_MONTH=100;
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int empHrs=0;
+	
+    void computeEmpWage() {
+    	int empHrs=0;
         int totalWorkingDays=0;
         int totalEmpHrs=0;
-		System.out.println("Welcome to Employee Wage computation");
-		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUM_OF_WORKING_DAYS) {
+        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUM_OF_WORKING_DAYS) {
 			totalWorkingDays++;
         int empcheck=(int)Math.floor(Math.random() * 10 )%3;
         switch (empcheck) {
@@ -30,5 +28,14 @@ public class EmpWage {
 	}
 		int totalEmpWage=totalEmpHrs * EMP_RATE_PER_HOUR;
         System.out.println("Total Emp Wage:" +totalEmpWage);
+    }
+}
+public class EmpWage {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Employee Wage computation");
+		EmpWageCalculate employee=new EmpWageCalculate();
+		employee.computeEmpWage();
 }
 }
