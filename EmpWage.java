72d35f8b83fcadd9b0dd2a635ef53249c8bad05e
kajal <tiwarikajal1306@gmail.com>
2020-05-12 20:29:14 +0530

    Compute EmpWage using class method

--- EmpWage.java
+++ EmpWage.java
@@ -1,12 +1,12 @@
 package javaPrograms;
-class EmpWageCalculate{
+public class EmpWage {
 	public static final int IS_PART_TIME=1;
 	public static final	int IS_FULL_TIME=2;
 	public static final int  EMP_RATE_PER_HOUR=20;
 	public static final int NUM_OF_WORKING_DAYS=20;
 	public static final int MAX_HRS_IN_MONTH=100;
 	
-    void computeEmpWage() {
+    public static int computeEmpWage() {
     	int empHrs=0;
         int totalWorkingDays=0;
         int totalEmpHrs=0;
@@ -22,20 +22,18 @@ class EmpWageCalculate{
                 break;
         default:
                 empHrs=0;
-        }
+      }
         totalEmpHrs +=empHrs;
         System.out.println("Day: " +totalWorkingDays + "Emp Hr:" +empHrs);
-	}
+	  }
 		int totalEmpWage=totalEmpHrs * EMP_RATE_PER_HOUR;
         System.out.println("Total Emp Wage:" +totalEmpWage);
-    }
-}
-public class EmpWage {
-	
+        return totalEmpWage;
+     }
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Employee Wage computation");
-		EmpWageCalculate employee=new EmpWageCalculate();
-		employee.computeEmpWage();
+		computeEmpWage();
 }
 }
